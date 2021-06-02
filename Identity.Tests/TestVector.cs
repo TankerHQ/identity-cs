9a87177c4474759d228526d53ccc136993f9c77b
Timothée Isnard <timothee.isnard@tanker.io>
2021-06-02 11:14:38 +0200

    feat: Serialize identities in stable JSON format, add test vectors

--- Identity.Tests/TestVector.cs
+++ Identity.Tests/TestVector.cs
@@ -43,7 +43,8 @@ namespace Tanker.Tests
             Encoding.UTF8.GetBytes(UserId), Convert.FromBase64String(TrustchainID));
         private const string PermanentIdentity = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSIsImRlbGVnYXRpb25fc2lnbmF0dXJlIjoiVTlXUW9sQ3ZSeWpUOG9SMlBRbWQxV1hOQ2kwcW1MMTJoTnJ0R2FiWVJFV2lyeTUya1d4MUFnWXprTHhINmdwbzNNaUE5cisremhubW9ZZEVKMCtKQ3c9PSIsImVwaGVtZXJhbF9wdWJsaWNfc2lnbmF0dXJlX2tleSI6IlhoM2kweERUcHIzSFh0QjJRNTE3UUt2M2F6TnpYTExYTWRKRFRTSDRiZDQ9IiwiZXBoZW1lcmFsX3ByaXZhdGVfc2lnbmF0dXJlX2tleSI6ImpFRFQ0d1FDYzFERndvZFhOUEhGQ2xuZFRQbkZ1Rm1YaEJ0K2lzS1U0WnBlSGVMVEVOT212Y2RlMEhaRG5YdEFxL2RyTTNOY3N0Y3gwa05OSWZodDNnPT0iLCJ1c2VyX3NlY3JldCI6IjdGU2YvbjBlNzZRVDNzMERrdmV0UlZWSmhYWkdFak94ajVFV0FGZXh2akk9In0=";
         private const string ProvisionalIdentity = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJlbWFpbCIsInZhbHVlIjoiYnJlbmRhbi5laWNoQHRhbmtlci5pbyIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Ii8yajRkSTNyOFBsdkNOM3VXNEhoQTV3QnRNS09jQUNkMzhLNk4wcSttRlU9IiwicHJpdmF0ZV9lbmNyeXB0aW9uX2tleSI6IjRRQjVUV212Y0JyZ2V5RERMaFVMSU5VNnRicUFPRVE4djlwakRrUGN5YkE9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJXN1FFUUJ1OUZYY1hJcE9ncTYydFB3Qml5RkFicFQxckFydUQwaC9OclRBPSIsInByaXZhdGVfc2lnbmF0dXJlX2tleSI6IlVtbll1dmRUYUxZRzBhK0phRHBZNm9qdzQvMkxsOHpzbXJhbVZDNGZ1cVJidEFSQUc3MFZkeGNpazZDcnJhMC9BR0xJVUJ1bFBXc0N1NFBTSDgydE1BPT0ifQ==";
-        private const string PublicIdentity = "eyJ0YXJnZXQiOiJ1c2VyIiwidHJ1c3RjaGFpbl9pZCI6InRwb3h5TnpoMGhVOUcyaTlhZ012SHl5ZCtwTzZ6R0NqTzlCZmhyQ0xqZDQ9IiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSJ9";
+        private const string PublicIdentity = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSJ9";
+        private const string PublicProvisionalIdentity = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJlbWFpbCIsInZhbHVlIjoiYnJlbmRhbi5laWNoQHRhbmtlci5pbyIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Ii8yajRkSTNyOFBsdkNOM3VXNEhoQTV3QnRNS09jQUNkMzhLNk4wcSttRlU9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJXN1FFUUJ1OUZYY1hJcE9ncTYydFB3Qml5RkFicFQxckFydUQwaC9OclRBPSJ9";
 
         [Test]
         public void ValidPermanentIdentity()
@@ -57,6 +58,7 @@ namespace Tanker.Tests
             Assert.That(identity.EphemeralPublicSignatureKey, Is.EqualTo(Convert.FromBase64String("Xh3i0xDTpr3HXtB2Q517QKv3azNzXLLXMdJDTSH4bd4=")));
             Assert.That(identity.EphemeralPrivateSignatureKey, Is.EqualTo(Convert.FromBase64String("jEDT4wQCc1DFwodXNPHFClndTPnFuFmXhBt+isKU4ZpeHeLTENOmvcde0HZDnXtAq/drM3Ncstcx0kNNIfht3g==")));
             Assert.That(identity.UserSecret, Is.EqualTo(Convert.FromBase64String(@"7FSf/n0e76QT3s0DkvetRVVJhXZGEjOxj5EWAFexvjI=")));
+            Assert.That(Utils.toBase64Json(identity), Is.EqualTo(PermanentIdentity));
         }
 
         [Test]
@@ -71,6 +73,7 @@ namespace Tanker.Tests
             Assert.That(identity.PrivateSignatureKey, Is.EqualTo(Convert.FromBase64String("UmnYuvdTaLYG0a+JaDpY6ojw4/2Ll8zsmramVC4fuqRbtARAG70Vdxcik6Crra0/AGLIUBulPWsCu4PSH82tMA==")));
             Assert.That(identity.PublicEncryptionKey, Is.EqualTo(Convert.FromBase64String("/2j4dI3r8PlvCN3uW4HhA5wBtMKOcACd38K6N0q+mFU=")));
             Assert.That(identity.PrivateEncryptionKey, Is.EqualTo(Convert.FromBase64String(@"4QB5TWmvcBrgeyDDLhULINU6tbqAOEQ8v9pjDkPcybA=")));
+            Assert.That(Utils.toBase64Json(identity), Is.EqualTo(ProvisionalIdentity));
         }
 
         [Test]
@@ -81,6 +84,20 @@ namespace Tanker.Tests
             Assert.That(identity.TrustchainId, Is.EqualTo(TrustchainID));
             Assert.That(identity.Target, Is.EqualTo("user"));
             Assert.That(identity.Value, Is.EqualTo(HashedUserId));
+            Assert.That(Utils.toBase64Json(identity), Is.EqualTo(PublicIdentity));
+        }
+
+        [Test]
+        public void ValidPublicProvisionalIdentity()
+        {
+            var identity = Utils.fromBase64Json<PublicProvisionalIdentity>(PublicProvisionalIdentity);
+
+            Assert.That(identity.TrustchainId, Is.EqualTo(TrustchainID));
+            Assert.That(identity.Target, Is.EqualTo("email"));
+            Assert.That(identity.Value, Is.EqualTo(UserEmail));
+            Assert.That(identity.PublicSignatureKey, Is.EqualTo(Convert.FromBase64String("W7QEQBu9FXcXIpOgq62tPwBiyFAbpT1rAruD0h/NrTA=")));
+            Assert.That(identity.PublicEncryptionKey, Is.EqualTo(Convert.FromBase64String("/2j4dI3r8PlvCN3uW4HhA5wBtMKOcACd38K6N0q+mFU=")));
+            Assert.That(Utils.toBase64Json(identity), Is.EqualTo(PublicProvisionalIdentity));
         }
     }
 }
--- Identity/ProvisionalIdentity.cs
+++ Identity/ProvisionalIdentity.cs
@@ -8,10 +8,15 @@ namespace Tanker
     [JsonObject(NamingStrategyType = typeof(SnakeCaseStrategy))]
     internal class PublicProvisionalIdentity
     {
+        [JsonProperty(Order = 1)]
         public string TrustchainId { get; set; }
+        [JsonProperty(Order = 2)]
         public string Target { get; set; }
+        [JsonProperty(Order = 3)]
         public string Value { get; set; }
+        [JsonProperty(Order = 5)]
         public byte[] PublicSignatureKey { get; set; }
+        [JsonProperty(Order = 4)]
         public byte[] PublicEncryptionKey { get; set; }
 
         public PublicProvisionalIdentity() { }
@@ -29,18 +34,19 @@ namespace Tanker
     [JsonObject(NamingStrategyType = typeof(SnakeCaseStrategy), ItemRequired = Required.Always)]
     internal class SecretProvisionalIdentity
     {
+        [JsonProperty(Order = 1)]
         public string TrustchainId { get; set; }
-
+        [JsonProperty(Order = 2)]
         public string Target { get; set; } = "email";
-
+        [JsonProperty(Order = 3)]
         public string Value { get; set; }
-
+        [JsonProperty(Order = 4)]
         public byte[] PublicEncryptionKey { get; set; }
-
+        [JsonProperty(Order = 5)]
         public byte[] PrivateEncryptionKey { get; set; }
-
+        [JsonProperty(Order = 6)]
         public byte[] PublicSignatureKey { get; set; }
-
+        [JsonProperty(Order = 7)]
         public byte[] PrivateSignatureKey { get; set; }
 
         public SecretProvisionalIdentity() { }
